36aa20cb005fd0bfc25984f72721fd1a268ebafc
unknown <felipereis.live@gmail.com>
2024-11-27 18:32:48 -0300

    Criando validação de token

--- src/main/java/com/example/login_auth_api/infra/security/TokenService.java
+++ src/main/java/com/example/login_auth_api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.example.login_auth_api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.example.login_auth_api.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -30,6 +31,22 @@ public class TokenService{
             return token;
         }catch (JWTCreationException e){
             throw new RuntimeException("Error generating token");
+        }
+    }
+
+    public String validateToken(String token){
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer("login-auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        }catch (JWTVerificationException e){
+            return null;
         }
     }
 
